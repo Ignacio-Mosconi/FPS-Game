d3def087df15884d466000763822607b15786bd6
Unknown <nacho7577@hotmail.com>
2018-06-01 11:59:17 -0300

    Created a Basic Terrain & Added a First Person Character

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerMovement : MonoBehaviour 
+{
+    [SerializeField] private float movementSpeed;
+    private CharacterController charController;
+
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    void Update() 
+	{
+        Vector3 movement = new Vector3(0, 0, 0);
+
+        float fwdMovement = Input.GetAxis("Vertical") * movementSpeed;
+        float horMovement = Input.GetAxis("Horizontal") * movementSpeed;
+
+        movement += transform.forward * fwdMovement + transform.right * horMovement;
+
+        charController.Move(movement * Time.deltaTime);
+	}
+}
