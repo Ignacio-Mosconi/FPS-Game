43dc1bcd0a0cc979e676aa00250974fe9ee9449d
Unknown <nacho7577@hotmail.com>
2018-06-18 19:18:14 -0300

    Improved Jumping

--- Assets/Scripts/Player/AnimationScript.cs
+++ Assets/Scripts/Player/AnimationScript.cs
@@ -21,9 +21,11 @@ public class AnimationScript : MonoBehaviour
 	{
         Vector3 horVelocity = new Vector3(charController.velocity.x, 0, charController.velocity.z);
         float normalizedVelocity = horVelocity.magnitude / playerMovement.MovementSpeed;
+        float verticalVelocity = charController.velocity.y;
         bool jumping = !charController.isGrounded;
 
         animator.SetFloat("Horizontal Velocity", normalizedVelocity, 0.2f, Time.deltaTime);
-        animator.SetBool("Jumping", jumping);
+        animator.SetFloat("Vertical Velocity", verticalVelocity, 0.2f, Time.deltaTime);
+        animator.SetBool("Is Jumping", jumping);
 	}
 }
